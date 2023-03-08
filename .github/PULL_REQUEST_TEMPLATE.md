4f52ce06038c8affad065acbf50de5334ebe95a6
wamartinezq <w.martinezq@uniandes.edu.co>
2023-03-07 19:12:09 -0500

    microservicio empaquetado con db

--- .github/PULL_REQUEST_TEMPLATE.md
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -1,5 +1,7 @@
 ## Describa el cambio
 +
+## Razonamiento pedagógico
++
 
 ### Lista de chequeo
 - [ ] Ejecuté el proyecto de forma local o usando Gitpod
---
+++ init.sql
@@ -0,0 +1,7 @@
+CREATE TABLE eda_empaquetado(  
+    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Llave primaria',
+    producto_id VARCHAR(255),
+    estado VARCHAR(255),
+    fecha VARCHAR(255)
+
+) COMMENT 'Tabla estado de los empaquetados';
